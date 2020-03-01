29c4308038eae16b57cfb3f8da5043b368688a80
Krystian Gagracz <krystian.gagracz@gmail.com>
2020-03-01 19:16:18 +0100

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let ghPages = require("gulp-gh-pages");
 
-gulp.task('sass', () => {
-    return gulp.src('app/scss/**/*.scss').pipe(sass()).pipe(gulp.dest('app/css'));
-
+gulp.task("sass", () => {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"));
 });
 
-gulp.task('watch', () => {
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+gulp.task("watch", () => {
+  gulp.watch("app/scss/**/*.scss", gulp.series("sass"));
+});
+
+gulp.task("deploy", function() {
+  return gulp.src("./dist/**/*").pipe(ghPages());
 });
