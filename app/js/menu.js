421aa1d8de9e6d4840dab646e49c26218491c0d0
Krystian Gagracz <krystian.gagracz@gmail.com>
2020-03-01 15:29:22 +0100

    menu-added show

--- app/js/menu.js
+++ app/js/menu.js
@@ -4,8 +4,37 @@ console.log(options);
 for (let i = 0; i <= options.length; i++) {
     options[i].addEventListener('click', function(){
         console.log('klik na ' + options[i].id);
-        let menuContainer = document.getElementsByClassName('menu-added');
-        console.log(menuContainer)
-        menuContainer.setAttribute('id', options[i].id);
+        document.querySelector('body').style.backgroundColor = 'rgba(15, 14, 14, 0.65)' ;
+        if (options[i].id == 'pizza') {
+            showMenu('pizza-display');
+        }
+        else if (options[i].id == 'pork') {
+            showMenu('pork-display');
+        }
+        else if (options[i].id == 'chicken') {
+            showMenu('chicken-display');
+        }
+        else if (options[i].id == 'pasta') {
+            showMenu('pasta-display');
+        }
+        else if (options[i].id == 'fish') {
+            showMenu('fish-display');
+        }
+        else if (options[i].id == 'bar') {
+            showMenu('bar-display');
+        }
+        else if (options[i].id == 'snacks') {
+            showMenu('snacks-display');
+        }
+        else if (options[i].id == 'salad') {
+            showMenu('salad-display');
+        }
+        else if (options[i].id == 'drink') {
+            showMenu('drink-display');
+        }
+        function showMenu (id) {
+            document.getElementById(id).style.display = 'block';
+            document.getElementById(id).style.opacity = '1';
+        }
     })
 };
