274ceeeb7c46b7fc673f53c0752588b16ba0223b
Krystian Gagracz <krystian.gagracz@gmail.com>
2020-03-01 02:13:58 +0100

    js

--- app/js/menu.js
+++ app/js/menu.js
@@ -4,7 +4,8 @@ console.log(options);
 for (let i = 0; i <= options.length; i++) {
     options[i].addEventListener('click', function(){
         console.log('klik na ' + options[i].id);
-        
-        
+        let menuContainer = document.getElementsByClassName('menu-added');
+        console.log(menuContainer)
+        menuContainer.setAttribute('')
     })
 };
